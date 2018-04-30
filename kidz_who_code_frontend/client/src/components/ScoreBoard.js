54f41bf177be679f99a9066829afb16fe88948c3
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-04-30 21:07:07 +0100

    Add ScoreBoard.js and import it into Quiz.js

---
+++ kidz_who_code_frontend/client/src/components/ScoreBoard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ScoreBoard = (props) => {
+  return(
+    <React.Fragment>
+      {props.currentScore}
+    </React.Fragment>
+  )
+}
+
+export default ScoreBoard;
--- kidz_who_code_frontend/client/src/containers/Quiz.js
+++ kidz_who_code_frontend/client/src/containers/Quiz.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Question from '../components/Question';
+import ScoreBoard from '../components/ScoreBoard';
 
 class Quiz extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      currentQuestion: null
+      currentQuestion: null,
+      currentScore: 0
     }
   }
 
@@ -14,19 +16,10 @@ class Quiz extends React.Component {
       <React.Fragment>
         <h1>Quiz</h1>
         <Question facts={this.props}/>
-        {/* <ul>
-          {this.props.facts[1].q_and_a.map(element =>
-          <div key={element.id}>
-          <li>{element.question}</li>
-          <li>{element.correct_answer}</li>
-          <li>{element.incorrect_answer_1}</li>
-          <li>{element.incorrect_answer_2}</li>
-        </div>
-      )}
-    </ul> */}
-  </React.Fragment>
-)
-}
+        <ScoreBoard currentScore={this.state.currentScore} />
+      </React.Fragment>
+    )
+  }
 }
 
 export default Quiz;
