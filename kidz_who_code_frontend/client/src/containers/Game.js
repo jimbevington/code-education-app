f5ef41860ae3bae1cb09d5ccefb9cbbdea2f656d
Jim Bevington <jim.bevington@hotmail.com>
2018-04-30 10:50:12 +0100

    Added go button and generatePlayerStartPos method to Game

--- kidz_who_code_frontend/client/src/containers/Game.js
+++ kidz_who_code_frontend/client/src/containers/Game.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Grid from '../components/Grid';
 import MovesContainer from '../components/MovesContainer'
 import MoveBoard from '../components/MoveBoard';
+import Go from '../components/Go';
 
 class Game extends React.Component {
   constructor(props){
@@ -13,6 +14,25 @@ class Game extends React.Component {
       playerPosition: null,
       moveList: []
     }
+    this.handleMove = this.handleMove.bind(this);
+  }
+
+  componentDidMount(){
+    // generate a random player position
+    const playerStart = this.generatePlayerStartPos();
+    debugger;
+    // generate a random goal position
+    // make updated cellStates
+    // setState of cellStates
+
+  }
+
+  generatePlayerStartPos = () => {
+    return Math.floor((Math.random() * this.props.squaredSize ** 2));
+  }
+
+  handleMove(){
+    return <p>Move Handled</p>
   }
 
   render(){
@@ -21,6 +41,7 @@ class Game extends React.Component {
         <MovesContainer squaredSize={this.props.squaredSize}/>
         <Grid squaredSize={this.props.squaredSize} cellStates={this.state.cellStates}/>
         <MoveBoard moves={this.state.moveList}/>
+        <Go handleClick={this.handleMove}/>
       </React.Fragment>
     )
   }
