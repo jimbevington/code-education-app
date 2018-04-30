c6845050ee435b9c13b8187819aa7c091a0d8d40
Jean BaMaung <jbamaung17@gmail.com>
2018-04-30 17:12:30 +0100

    added an Information box

--- kidz_who_code_frontend/client/src/components/Welcome.js
+++ kidz_who_code_frontend/client/src/components/Welcome.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import DownArrow from "./public/downarrow.png";
 import Alien from "./public/alien.jpg";
+import InfoBox from './InfoBox';
 import './Welcome.css';
 
 const Welcome = (props) => {
@@ -8,7 +9,7 @@ const Welcome = (props) => {
     <div id="welcome">
       <h1>This is the welcome text</h1>
       <section className="alien">
-      <img src={Alien} alt="this is an alien" width="200" height="200"/>
+      <img id="alienpic" src={Alien} alt="this is an alien" width="200" height="200"/>
 
       <form method="get">
       <p id="speechbubble">This is the text to go in the speech bubble. What's your name?</p>
@@ -16,10 +17,11 @@ const Welcome = (props) => {
       <input type="submit" name="submitbutton" value="Let's get Divvy home!" />
     </form>
       </section>
-    <section className="infobox">
+      <InfoBox text='This is where all the words in the info box are going to go' />
+    {/* <section className="infobox">
       <h2 id="infobox-heading">Info Box Heading</h2>
       <p>This is where all the words in the info box are going to go</p>
-    </section>
+    </section> */}
     <img src={DownArrow} width="200" height="200" alt="this should be a down arrow" />
     </div>
   )
