b055b69cf4756996d1d4ea7e1406d445575d3243
Jean BaMaung <jbamaung17@gmail.com>
2018-04-30 17:01:23 +0100

    Got a speech bubble looking like a speechbubble

--- kidz_who_code_frontend/client/src/components/Welcome.js
+++ kidz_who_code_frontend/client/src/components/Welcome.js
@@ -5,13 +5,18 @@ import './Welcome.css';
 
 const Welcome = (props) => {
   return(
-    <div>
+    <div id="welcome">
       <h1>This is the welcome text</h1>
-      <section class="alien">
+      <section className="alien">
       <img src={Alien} alt="this is an alien" width="200" height="200"/>
-      <p id="speechbubble">This is the text to go in the speech bubble</p>
+
+      <form method="get">
+      <p id="speechbubble">This is the text to go in the speech bubble. What's your name?</p>
+      <input type="text" name="name" />
+      <input type="submit" name="submitbutton" value="Let's get Divvy home!" />
+    </form>
       </section>
-    <section class="infobox">
+    <section className="infobox">
       <h2 id="infobox-heading">Info Box Heading</h2>
       <p>This is where all the words in the info box are going to go</p>
     </section>
