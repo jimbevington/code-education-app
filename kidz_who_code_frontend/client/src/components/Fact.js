ce97d36724ad18e2e25e920f3749ecb81faf1ae4
Emma Roberts <emrsixtyseven@gmail.com>
2018-05-01 16:11:51 +0100

    speech bubble added

--- kidz_who_code_frontend/client/src/components/Fact.js
+++ kidz_who_code_frontend/client/src/components/Fact.js
@@ -6,12 +6,14 @@ import Order from "./Order.js";
 import Bug from "./Bug.js";
 import Arrow from './Arrow';
 import Alien from './Alien.js'
+import './Fact.css'
+
 
 
 const Fact = (props) => {
   if(props.length === 0) return null;
   return (
-    <div className='facts-container'>
+    <div id='facts-container'>
       <Arrow direction="upArrow" link="Intro"/>
       <h1>Fun Facts!</h1>
       <CompInfo facts={props} />
@@ -21,6 +23,7 @@ const Fact = (props) => {
       <Bug facts={props} />
       <Arrow direction="downArrow" link="Game"/>
       <Alien />
+      <p id="speechbubble">Let's start learning! Click on each button find out more</p>
     </div>
   );
 
