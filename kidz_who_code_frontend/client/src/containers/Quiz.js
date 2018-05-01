1bdb812c7b7583d677f0d429c0b565e8f4bd3985
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-05-01 11:14:47 +0100

    Add handleSubmit() to Quiz.js with basic functionality to increment question and score

--- kidz_who_code_frontend/client/src/containers/Quiz.js
+++ kidz_who_code_frontend/client/src/containers/Quiz.js
@@ -7,13 +7,33 @@ class Quiz extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      currentQuestion: 0,
-      currentScore: 0
+      currentQuestion: null,
+      currentScore: null
     }
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount(){
+    this.setState({
+      currentQuestion: 0,
+      currentScore: 0
+    });
+  }
 
+  handleSubmit(){
+    // get the current question
+    // check if answer correct
+      // if correct
+        // increment 'currentScore'
+        this.setState(prevState => ({
+          currentQuestion: prevState.currentQuestion + 1,
+          currentScore: prevState.currentScore + 1
+        }));
+        // add/change class on answer text to colour green
+        // display 'next question' button
+      // if incorrect
+        // add/change class on answer text to colour red
+        // allow user to select another answer
   }
 
   render(){
@@ -23,6 +43,8 @@ class Quiz extends React.Component {
         {console.log(this.props)}
         <Question facts={this.props}/>
         <SubmitAnswer handleClick={this.handleSubmit} />
+        {console.log('currentQuestion', this.state.currentQuestion)}
+        {console.log('currentScore', this.state.currentScore)}
         // Add the three answer options
         <ScoreBoard currentScore={this.state.currentScore} />
       </React.Fragment>
