86f8b8e7265123f2cb4ac79b97d2bdd63c971f50
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-05-02 08:14:07 +0100

    Add handleAnswerClick() to Quiz.js

--- kidz_who_code_frontend/client/src/containers/Quiz.js
+++ kidz_who_code_frontend/client/src/containers/Quiz.js
@@ -2,17 +2,16 @@ import React from 'react';
 import Question from '../components/Question';
 import ScoreBoard from '../components/ScoreBoard';
 import SubmitAnswer from '../components/SubmitAnswer';
-import Arrow from '../components/Arrow';
-import './Quiz.css';
 
 class Quiz extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      currentQuestion: 0,
-      currentScore: 0
+      currentQuestion: 0
+
     }
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleAnswerClick = this.handleAnswerClick.bind(this);
   }
 
   handleSubmit(){
@@ -23,7 +22,6 @@ class Quiz extends React.Component {
     if(this.state.currentQuestion < this.props.facts[1].q_and_a.length){
       this.setState(prevState => ({
         currentQuestion: prevState.currentQuestion + 1,
-        currentScore: prevState.currentScore + 1
       }));
     }
     // add/change class on answer text to colour green
@@ -33,19 +31,30 @@ class Quiz extends React.Component {
     // allow user to select another answer
   }
 
+  handleAnswerClick(e){
+    // console.log(e.target.value);
+    if(this.props.facts[1].q_and_a[this.state.currentQuestion].correct_answer === e.target.value){
+      console.log('correct answer');
+      // toggle button class to display green
+    } else {
+      console.log('incorrect answer');
+      // toggle button class to display red
+    }
+  }
+
   render(){
     return(
-      <section id="quiz page">
-        <Arrow direction="upArrow" link="Game" />
+      <React.Fragment>
         <h1>Quiz</h1>
         <Question
           qAndAs={this.props.facts[1].q_and_a}
           currentQuestion={this.state.currentQuestion}
+          handleAnswerClick={this.handleAnswerClick}
         />
         <SubmitAnswer handleClick={this.handleSubmit} />
-
-        <Arrow direction="downArrow" link="Bye" />
-      </section>
+        // display ScoreBoard only when all questions have been answered
+        {/* <ScoreBoard currentScore={this.state.currentScore} /> */}
+      </React.Fragment>
     )
   }
 }
