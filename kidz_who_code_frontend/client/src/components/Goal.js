73c1a977603495036cc0698bb63c3b2676d5d17d
Jim Bevington <jim.bevington@hotmail.com>
2018-05-02 10:15:55 +0100

    fixed resizing Game board

--- kidz_who_code_frontend/client/src/components/Goal.js
+++ kidz_who_code_frontend/client/src/components/Goal.js
@@ -2,7 +2,7 @@ import React from 'react';
 import moon from './public/moon.svg';
 
 const Goal = () => {
-  return <img className='game-actor' src={moon} alt="Divvy's Home"/>
+  return <img className='game-actor' src={moon} alt="Divvy's Home" width='75px'/>
 }
 
 export default Goal;
