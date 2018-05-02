ffe4a39aee23a6e203081ac9d6e7014304fdec74
Emma Roberts <emrsixtyseven@gmail.com>
2018-05-02 20:13:19 +0100

    certificate added to background

--- kidz_who_code_frontend/client/src/components/Bye.js
+++ kidz_who_code_frontend/client/src/components/Bye.js
@@ -4,21 +4,24 @@ import Alien from './Alien';
 import SpeechBubble from './SpeechBubble';
 import LinkImage from './LinkImage';
 import codeclan from './public/codeclan-logo.png';
+import Certificate from "./public/Certificate.png";
+// import './Bye.css'
 
 const Bye = () => {
 
-  const wellDone = 'Well Done! You\'re now a Junior Developer!';
-  const signUp = 'You\'re ready for your next challenge';
-
-  const bubbleContents = [wellDone, signUp];
+  // const wellDone = 'Well Done! You\'re now a Junior Developer!';
+  // const signUp = 'You\'re ready for your next challenge';
+  //
+  // const bubbleContents = [wellDone, signUp];
 
   return (
-    <section id="bye" className="page">
+    <section id="bye" className="byepage">
       <header>
         <Arrow direction='upArrow' link='Quiz' />
       </header>
-      <Alien floatStyle="float-none" class='alienpic'/>
-      <section className='column' style={{'alignSelf': 'center'}}>
+      <Alien floatStyle="float-none" class='alienpic'/> 
+
+      {/* <section className='column' style={{'alignSelf': 'center'}}>
         <SpeechBubble paras={bubbleContents} width='300px'/>
         <LinkImage
           href="https://codeclan.com/"
@@ -26,7 +29,7 @@ const Bye = () => {
           alt="Codeclan Logo"
           width='400px'
         />
-      </section>
+      </section> */}
       <Arrow direction="rightArrow" link='Intro' />
     </section>
   )
